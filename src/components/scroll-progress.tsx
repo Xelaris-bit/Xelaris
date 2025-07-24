ff0c4c2d1537bc8e8fe8183dfbbc3c41be88f941
Anshuman Sahoo <anshumanseoczar@gmail.com>
2025-07-24 17:46:14 +0000

    Its not properly visible move it to forward

--- src/components/scroll-progress.tsx
+++ src/components/scroll-progress.tsx
@@ -6,7 +6,7 @@ export const ScrollProgress = () => {
     const { scrollYProgress } = useScroll();
     return (
         <motion.div
-            className="fixed top-0 left-0 right-0 h-1 bg-accent origin-[0%] z-50"
+            className="fixed top-0 left-0 right-0 h-1 bg-accent origin-[0%] z-[100]"
             style={{ scaleX: scrollYProgress }}
         />
     )
