504c5750d973cf99f15e3484b5ab816a081ddfd2
Anshuman Sahoo <anshumanseoczar@gmail.com>
2025-07-24 17:44:17 +0000

    No don't remove that

---
+++ src/components/scroll-progress.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { motion, useScroll } from 'framer-motion';
+
+export const ScrollProgress = () => {
+    const { scrollYProgress } = useScroll();
+    return (
+        <motion.div
+            className="fixed top-0 left-0 right-0 h-1 bg-accent origin-[0%] z-50"
+            style={{ scaleX: scrollYProgress }}
+        />
+    )
+}
