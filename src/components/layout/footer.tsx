b43c60cd88e41a2b2e23c90fcf1a190a3aed14e8
Anshuman Sahoo <anshumanseoczar@gmail.com>
2025-07-24 17:13:39 +0000

    provide a small gap between them

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <div className="flex flex-col gap-4 items-start">
              <Link href="/" className="flex items-center gap-0 font-bold text-2xl">
                 <Logo className="h-8 w-8 text-accent" />
-                <span className="font-headline -ml-2">elaris</span>
+                <span className="font-headline">elaris</span>
             </Link>
             <p className="text-sm text-secondary-foreground/80">Xelaris a Leading QA Company Providing Top-Tier Software Testing Solutions.</p>
             <div className="flex space-x-4">
