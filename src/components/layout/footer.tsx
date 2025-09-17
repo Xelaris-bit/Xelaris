e529865f0129e75b86858494db23567362d5e0b3
Anshuman Sahoo <anshumanseoczar@gmail.com>
2025-09-17 16:43:28 +0000

    For iPad pro "Quick Links Resources & Contact" align it towards left by

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
               <Link href="#" aria-label="LinkedIn" className="text-secondary-foreground/80 hover:text-accent transition-colors"><Linkedin /></Link>
             </div>
           </div>
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             <div>
               <h4 className="font-semibold text-lg mb-4">Quick Links</h4>
               <ul className="space-y-3 antialiased">
