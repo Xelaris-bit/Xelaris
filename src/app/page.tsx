387a7ee489b726f8af79d8991b7135ca2d9db211
Anshuman Sahoo <anshumanseoczar@gmail.com>
2025-09-18 10:39:43 +0000

    Remove "contact us" & "our QA services" From home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 import HeroSection from '@/components/sections/hero';
 import WhyChooseUsSection from '@/components/sections/why-choose-us';
-import ServicesSection from '@/components/sections/services';
 import TestimonialsSection from '@/components/sections/testimonials';
 import { FadeIn } from '@/components/fade-in';
 import RecentCaseStudiesSection from '@/components/sections/recent-case-studies';
-import ContactSection from '@/components/sections/contact';
 
 export default function Home() {
   return (
@@ -15,10 +13,8 @@ export default function Home() {
       <main className="flex-grow">
         <HeroSection />
         <FadeIn><WhyChooseUsSection /></FadeIn>
-        <FadeIn><ServicesSection /></FadeIn>
         <FadeIn><RecentCaseStudiesSection /></FadeIn>
         <FadeIn><TestimonialsSection /></FadeIn>
-        <FadeIn><ContactSection /></FadeIn>
       </main>
       <Footer />
     </div>
