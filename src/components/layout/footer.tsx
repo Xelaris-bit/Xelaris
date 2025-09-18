641c8ce0eccc923375d4477e87557920b45ff663
Anshuman Sahoo <anshumanseoczar@gmail.com>
2025-09-18 09:51:42 +0000

    Write it like Xelaris provides (_for element p_)

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
                 <Logo className="h-8 w-8 text-accent" />
                 <span className="font-headline -ml-1">elaris</span>
             </Link>
-            <p className="text-sm text-secondary-foreground/80 antialiased">Expert IT solutions across software development, eLearning, multimedia, QA, and digital marketing.</p>
+            <p className="text-sm text-secondary-foreground/80 antialiased">Xelaris provides expert IT solutions across software development, eLearning, multimedia, QA, and digital marketing.</p>
             <div className="flex space-x-4">
               <Link href="#" aria-label="Twitter" className="text-secondary-foreground/80 hover:text-accent transition-colors"><Twitter /></Link>
               <Link href="#" aria-label="Facebook" className="text-secondary-foreground/80 hover:text-accent transition-colors"><Facebook /></Link>
